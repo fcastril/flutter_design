4509652d70f0997f0ad68c18cc6d8d449e29c437
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2023-01-13 14:18:05 -0500

    continuar con el slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
+import 'package:disenos/src/pages/slidesshow_page.dart';
 import 'package:flutter/material.dart';
 
-
-import 'package:disenos/src/pages/graficas_circulares_page.dart';
 // import 'package:disenos/src/pages/headers_page.dart';
 // import 'package:disenos/src/pages/animaciones_page.dart';
 // import 'package:disenos/src/retos/cuadrado_animado_page.dart';
- 
-
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Diseños App',
-      home: GraficasCircularesPage()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Diseños App',
+        home: SlideShowPage());
   }
 }
